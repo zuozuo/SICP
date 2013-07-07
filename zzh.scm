e5c3ea7791a285d223665b6438963972766643ee
zuozuo <zzhattzzh@126.com>
2013-07-07 16:28:16 -0700

    #

--- zzh.scm
+++ zzh.scm
@@ -1,17 +1,63 @@
-(define (good-enough? guess pre_guess) 
-  (< (abs (- pre_guess guess)) 0.001))
-(define (sqrt-iter guess pre_guess x) 
-  (if (good-enough? guess pre_guess)
-      guess
-      (sqrt-iter (improve guess x) guess x)))
 
-(define (square x) (* x x))
-(define (improve guess x)
-  (average guess (/ x guess)))
-(define (average x y)
-  (/ (+ x y) 2))
 
-(define (sqrt x)
-  (sqrt-iter 1.0 x x))
 
-(display (square 0.03))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(define (multiply a b) 
+  (multiply-iter a b 0))
+
+(define (double x) (+ x x))
+(define (halve x) (/ x 2))
+
+(define (multiply-iter a b prod) 
+  (display b)
+  (newline)
+  (display prod)
+  (newline)
+  (display "*************")
+  (newline)
+  (cond ((= b 0) prod)
+        ((even? b) (multiply-iter (double a) (halve b) prod))
+        (else (multiply-iter a (- b 1) (+ prod a)))))
+
+(display (multiply 28 300))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
